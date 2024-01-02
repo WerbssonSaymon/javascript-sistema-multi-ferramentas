a96136696c445fe5e4bc1490eacaa603433a7384
WerbssonSaymon <saymonluz18@gmail.com>
2024-01-02 14:11:07 -0300

    edição das funções de conersão de temperatura

--- conversor.js
+++ conversor.js
@@ -1,44 +1,4 @@
-// Função que converte celsius para fahrenheit
-// function celsiusFahrenheit(celsius){
-//      let fahrenheit = (1.8 * celsius) + 32;
-//      return fahrenheit
-// }
-
-// Função que converte celsius para kelvin
-function celsiusKelvin(celsius){
-     let kelvin = celsius + 273.15;
-     return kelvin
-}
-
-// Função que converte kelvin para fahrenheit
-function kelvinFahrenheit(kelvin){
-     let result = ((kelvin - 273.15) * 9) / 5 + 32;
-     let fahrenheit = parseFloat(result.toFixed(3))
-     return fahrenheit
-}
-
-// Função que converte kelvin para celsius
-function kelvinCelsius(kelvin){
-     let result = kelvin - 273.15;
-     let celsius = parseFloat(result.toFixed(3))
-     return celsius
-}
-
-// Função que converte fahrenheit para celsius
-function fahrenheitCelsius(fahrenheit){
-     let result = ((fahrenheit - 32) * 5) / 9;
-     let celsius = parseFloat(result.toFixed(2))
-     return celsius
-}
-
-// Função que converte fahrenheit para kelvin
-function fahrenheitKelvin(fahrenheit){
-     let result = (((fahrenheit - 32) * 5) / 9) + 273.15;
-     let kelvin = parseFloat(result.toFixed(3))
-     return kelvin
-}
-
-// Área de manipulação dos formularios
+// Funções de conversões de graus de temperatura
 
 function celsiusFahrenheit(){
      let campoCelsius = document.getElementById('campoCelsius').value;
@@ -51,3 +11,61 @@ function celsiusFahrenheit(){
      }
 }
 
+function celsiusKelvin(){
+     let campoCelsius = document.getElementById('campoCelsius2').value;
+     let celsius = parseFloat(campoCelsius);
+     if (!isNaN(celsius)) {
+          let kelvin = celsius + 273.15;
+          document.getElementById('resultadoCelKel').value = kelvin;
+     } else {
+          alert('problema com o valor enviado');
+     } 
+}
+
+function kelvinFahrenheit(){
+     let campoKelvin = document.getElementById('campoKelvin').value;
+     let kelvin = parseFloat(campoKelvin);
+     if (!isNaN(kelvin)) {
+          let result = ((kelvin - 273.15) * 9) / 5 + 32;
+          let fahrenheit = parseFloat(result.toFixed(3))
+          document.getElementById('resultadoKelFah').value = fahrenheit;
+     } else {
+          alert('problema com o valor enviado');
+     } 
+}
+
+function kelvinCelsius(){
+     let campoKelvin = document.getElementById('campoKelvin2').value;
+     let kelvin = parseFloat(campoKelvin);
+     if (!isNaN(kelvin)) {
+          let result = kelvin - 273.15;
+          let celsius = parseFloat(result.toFixed(3))
+          document.getElementById('resultadoKelCel').value = celsius;
+     } else {
+          alert('problema com o valor enviado');
+     } 
+}
+
+function fahrenheitCelsius(){
+     let campoFahrenheit = document.getElementById('campoFahrenheit').value;
+     let fahrenheit = parseFloat(campoFahrenheit);
+     if (!isNaN(fahrenheit)) {
+          let result = ((fahrenheit - 32) * 5) / 9;
+          let celsius = parseFloat(result.toFixed(2))
+          document.getElementById('resultadoFahCel').value = celsius;
+     } else {
+          alert('problema com o valor enviado');
+     } 
+}
+
+function fahrenheitKelvin(){
+     let campoFahrenheit = document.getElementById('campoFahrenheit2').value;
+     let fahrenheit = parseFloat(campoFahrenheit);
+     if (!isNaN(fahrenheit)) {
+          let result = (((fahrenheit - 32) * 5) / 9) + 273.15;
+          let kelvin = parseFloat(result.toFixed(3))
+          document.getElementById('resultadoFahKel').value = kelvin;
+     } else {
+          alert('problema com o valor enviado');
+     } 
+}
